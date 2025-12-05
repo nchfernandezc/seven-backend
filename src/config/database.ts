5a6f2d8a5e98ac3e9841c49a53adbf438adb4805
Nairuma Fernandez <nfernandez@fibex.local>
2025-12-05 12:39:08 -0400

    configuración para bdd

--- src/config/database.ts
+++ src/config/database.ts
@@ -4,15 +4,16 @@ import { config } from "dotenv";
 config();
 
 export const AppDataSource = new DataSource({
-  type: 'postgres', 
-  host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT || '3306'),
-  username: process.env.DB_USERNAME || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_DATABASE || 'sistema',
-  synchronize: true, 
-  logging: process.env.DB_LOGGING === 'true',
-  entities: [__dirname + "/../entities/*.{js,ts}"],
-  // migrations: ['src/migrations/*.ts'],
-  // subscribers: ['src/subscribers/*.ts'],
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '5432'),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  synchronize: process.env.NODE_ENV !== 'production' || process.env.DB_SYNCHRONIZE === 'true',
+  logging: process.env.NODE_ENV !== 'production',
+  ssl: process.env.NODE_ENV === 'production' ? {
+    rejectUnauthorized: false
+  } : false,
+  entities: [__dirname + "/../entities/*.{js,ts}"]
 });
